3796cdba3756180d6296f7f69704d29e7df86144
Danko <bogdbutk-ipt28@lll.kpi.ua>
2026-04-17 23:17:23 +0300

    Add project files.

---
+++ lab1_NTA/lab1_NTA.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    // 1. Тест на простоту Міллера-Рабіна 
+    // Обрано, бо простіша реалізація і краща точність 
+
+
+
+
+
+    return 0;
+}
+
